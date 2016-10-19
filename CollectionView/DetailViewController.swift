dc1b67e27b38e6c9d7d7e684a968a75a987cdaeb
aka1969 <a.kravchenko.dr@gmail.com>
2016-10-18 23:48:09 -0400

    v01
    version 01

---
+++ CollectionView/DetailViewController.swift
@@ -0,0 +1,50 @@
+//
+//  DetailViewController.swift
+//  CollectionView
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    
+    @IBOutlet weak var DetailImage: UIImageView!
+    
+    
+    var name: AnyObject? {
+        
+        get {
+            return UserDefaults.standard.object(forKey: "name") as AnyObject?
+        }
+        
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        
+        DetailImage.image = UIImage(named: name as! String)
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- CollectionView/ViewController.swift
+++ CollectionView/ViewController.swift
@@ -2,24 +2,77 @@
 //  ViewController.swift
 //  CollectionView
 //
-//  Created by Aaron on 30/07/2015.
-//  Copyright © 2015 Aaron. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    
+    var array:[String] = []
+    
+    var name: AnyObject? {
+        
+        get {
+            return UserDefaults.standard.object(forKey: "name") as AnyObject?
+        }
+        set {
+            UserDefaults.standard.set(newValue!, forKey: "name")
+            UserDefaults.standard.synchronize()
+        }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        array = ["1capricorn.jpg","2aquarius.jpg","3pisces.jpg","4aries.jpg","5taurus.jpg","6gemini.jpg","7cancer.jpg","8leo.jpg","9virgo.jpg","10libra.jpg","11scorpio.jpg","12sagittarius.jpg"]
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+        
+        cell.Imageview.image = UIImage(named: array[(indexPath as NSIndexPath).row])
+        
+        return cell
+        
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        name = array [(indexPath as NSIndexPath).row] as AnyObject?
+        
+    }
+    
+    
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
